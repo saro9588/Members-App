d25bf694d8b5e4b1a6399222d6aa9d83b31a2b16
Saro <babikiansaro@gmail.com>
2023-11-26 14:18:09 -0800

    initial commit

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const AllUsers = () => {
+  return (
+    <>
+      <div>AllUsers</div>
+      <Link href="/">Back</Link>
+    </>
+  );
+};
+
+export default AllUsers;
