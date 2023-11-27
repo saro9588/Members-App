af83f0ed0bc878da21d2f0cc370549df19e56e87
Saro <babikiansaro@gmail.com>
2023-11-26 20:01:04 -0800

    All users table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
@@ -6,6 +6,35 @@ const AllUsers = () => {
   return (
     <>
       <div>All Users</div>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Age</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Years Joined</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+            <Table.Cell>10</Table.Cell>
+            <Table.Cell>2</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+            <Table.Cell>zahra@example.com</Table.Cell>
+            <Table.Cell>Admin</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+            <Table.Cell>jasper@example.com</Table.Cell>
+            <Table.Cell>Developer</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>
       <Button>
         <Link href="/">Back</Link>
       </Button>
