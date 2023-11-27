71a74b40216cefb398b7ff2e7ce781f15b210056
Saro <babikiansaro@gmail.com>
2023-11-27 10:45:01 -0800

    create the user model in prisma

---
+++ prisma/migrations/20231127182312_create_user/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstname` VARCHAR(25) NOT NULL,
+    `lastname` VARCHAR(25) NOT NULL,
+    `notes` TEXT NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `createdAT` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAT` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
