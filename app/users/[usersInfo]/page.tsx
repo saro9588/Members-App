0e50766535092c5b1f6cc1d066acae9a995247e4
Saro <babikiansaro@gmail.com>
2023-11-29 23:42:03 -0800

    userInfo page

--- app/users/[usersInfo]/page.tsx
+++ app/users/[usersInfo]/page.tsx
@@ -1,26 +1,57 @@
+"use client";
 import { Table } from "@radix-ui/themes";
+import Link from "next/link";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import { Button } from "@radix-ui/themes";
+import axios from "axios";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const SimpleMdeEditor = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const [users, setUsers] = useState<UserForm[]>([]);
+  interface UserForm {
+    firstName: string;
+    lastName: string;
+    notes: string;
+  }
+  const {
+    register,
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserForm>({});
+  const onSubmit = handleSubmit(async (data) => {
+    await axios.post("/api/users", data);
+    reset();
+  });
   return (
     <>
-      <h1>This is a sample Data</h1>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
+      <h1>User detailed notes</h1>
+      <h2>
+        {users.map((user) => (
+          <h1>{user.firstName} Hello</h1>
+        ))}
+      </h2>
 
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-            <Table.Cell>danilo@example.com</Table.Cell>
-            <Table.Cell>Developer</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table.Root>
+      <form className="max-w-xl space-y-3 mb-5" onSubmit={onSubmit}>
+        <Controller
+          name="notes"
+          control={control}
+          render={({ field }) => (
+            <SimpleMdeEditor placeholder="notes..." {...field} />
+          )}
+        />
+        <p>{errors.notes?.message}</p>
+        <Button type="submit">Save</Button>
+      </form>
+      <Button className="">
+        <Link href="/users">All Users</Link>
+      </Button>
     </>
   );
 }
