fdfccec5b95461ae1ce3b22e63d80b59f98c3501
Saro <babikiansaro@gmail.com>
2023-12-12 09:47:24 -0800

    change relation mode to prisma

--- prisma/migrations/20231212174606_initial_migration/migration.sql
+++ prisma/migrations/20231212174606_initial_migration/migration.sql
@@ -1,21 +1,21 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Users`;
-
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `firstname` VARCHAR(25) NOT NULL,
     `lastname` VARCHAR(25) NOT NULL,
-    `notes` TEXT NOT NULL,
+    `info` TEXT NOT NULL,
     `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
     `createdAT` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAT` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Note` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` TEXT NOT NULL,
+    `authorId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
