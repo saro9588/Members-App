cce50d3b8fd3f80557f1b85e3b039e0193f2ed13
Saro <babikiansaro@gmail.com>
2023-12-12 11:30:06 -0800

    App Description

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <>
       <h1>Welcome!</h1>
+      <p>
+        This App is for adding members and crearting notes unique to each member
+      </p>
       <Button className="">
         <Link href="/members">All Members</Link>
       </Button>
