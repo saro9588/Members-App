8b2a143130762eb8c02a34b32b0cd3a0a82fecb9
Saro <babikiansaro@gmail.com>
2023-12-13 16:40:47 -0800

    new migration

--- prisma/migrations/20231214001920_migration2/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX `Account_userId_idx` ON `Account`(`userId`);
-
--- CreateIndex
-CREATE INDEX `Note_authorId_idx` ON `Note`(`authorId`);
-
--- CreateIndex
-CREATE INDEX `Session_userId_idx` ON `Session`(`userId`);
