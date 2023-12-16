c0850224820d04183eddb2e31e55e04c50a91b5c
Saro <babikiansaro@gmail.com>
2023-12-15 19:52:51 -0800

    add postgresql table and row

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -3,35 +3,18 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const userEmail = searchParams.get("userEmail");
+  const userPassword = searchParams.get("userPassword");
+
   try {
-    const result =
-      await sql`CREATE TABLE Users ( Name varchar(255), password varchar(255) );`;
-    return NextResponse.json({ result }, { status: 200 });
+    if (!userEmail || !userPassword)
+      throw new Error("Pet and owner names required");
+    await sql`INSERT INTO Users (email, password) VALUES (${userEmail}, ${userPassword});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
-}
 
-export async function POST(request: Request) {
-  try {
-    const { email, password } = await request.json();
-    const hashedPassword = await hash(password, 10);
-    const dbRes = await sql`
-            INSERT INTO users (email, password)
-            VALUES (${email}, ${hashedPassword})
-        `;
-    return NextResponse.json(
-      { message: "User created successfully." },
-      {
-        status: 201,
-      }
-    );
-  } catch (e) {
-    return NextResponse.json(
-      { error: "An error occurred while processing your request." },
-      {
-        status: 500,
-      }
-    );
-  }
+  const pets = await sql`SELECT * FROM Pets;`;
+  return NextResponse.json({ pets }, { status: 200 });
 }
