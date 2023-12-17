ff1023a196535506e8296cd49ec8298ee9e25725
Saro <babikiansaro@gmail.com>
2023-12-16 18:16:25 -0800

    refactor route.js at register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -3,19 +3,36 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const email = searchParams.get("email");
-  const password = searchParams.get("password");
-
   try {
-    if (!email || !password) throw new Error("User and email are required");
+    const { email, password } = await request.json();
     const hashedPassword = await hash(password, 10);
-
-    await sql`INSERT INTO Users (email, password) VALUES (${email}, ${hashedPassword});`;
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    const dbRes = await sql`
+            INSERT INTO users (email, password)
+            VALUES (${email}, ${hashedPassword})
+        `;
+    return NextResponse.json(
+      { message: "User created successfully." },
+      {
+        status: 201,
+      }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { error: "An error occurred while processing your request." },
+      {
+        status: 500,
+      }
+    );
   }
-
-  const users = await sql`SELECT * FROM Users;`;
-  return NextResponse.json({ users }, { status: 200 });
 }
+
+//     if (!email || !password) throw new Error("User and email are required");
+
+//     await sql`INSERT INTO Users (email, password) VALUES (${email}, ${hashedPassword});`;
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 });
+//   }
+
+//   const users = await sql`SELECT * FROM Users;`;
+//   return NextResponse.json({ users }, { status: 200 });
+// }
