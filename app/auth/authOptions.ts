22a47f4f8c1979e880164f3c2cc61843114ebcb5
Saro <babikiansaro@gmail.com>
2023-12-16 17:49:01 -0800

    uncomment the provider.tsx

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,4 +1,4 @@
-import GoogleProvider from "next-auth/providers/google";
+//import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
 import { NextAuthOptions } from "next-auth";
