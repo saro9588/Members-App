91191bb08339e8176adc53c4b702021d9be965ca
Saro <babikiansaro@gmail.com>
2024-02-21 12:29:31 +0400

    render note on edit note page

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,11 +1,67 @@
+"use client";
+import { Button, TextArea } from "@radix-ui/themes";
 import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Note } from "@prisma/client";
 
-const EditNotePage = () => {
+const EditNoteForm = ({ id, note }: { id: number; note: Note }) => {
+  const router = useRouter();
+
+  interface MemberNote {
+    id: number;
+    description: string;
+    authorId: number;
+  }
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MemberNote>({});
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const { data: newNote } = await axios.post(`/api/members/${id}/`, data, {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      });
+      router.push(`/members/${newNote.id}/`);
+      console.log(newNote.id);
+    } catch (error) {
+      console.error(error);
+    }
+  });
   return (
-    <div className="grid grid-col-1 mx-auto max-w-screen-lg gap-2">
-      <p>Page coming soon...</p>
+    <div>
+      <form className="max-w-xl" onSubmit={onSubmit}>
+        <TextArea
+          {...register("description", { required: "This is required." })}
+          placeholder="Take notes..."
+          defaultValue={note.description}
+        />
+        <Button color="indigo" variant="soft" className="mt-2">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export default EditNoteForm;
 
-export default EditNotePage;
+// import React from "react";
+
+// const EditNotePage = () => {
+//   return (
+//     <div className="grid grid-col-1 mx-auto max-w-screen-lg gap-2">
+//       <p>Page coming soon...</p>
+//     </div>
+//   );
+// };
+
+// export default EditNotePage;
