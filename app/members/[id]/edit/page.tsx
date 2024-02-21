d528c04ccdcb47484a393ecd5c3fb83e6f0dbe59
Saro <babikiansaro@gmail.com>
2024-02-21 13:21:43 +0400

    render member existing note on note edit page

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,53 +1,9 @@
 "use client";
-import { Button, TextArea } from "@radix-ui/themes";
+import { Note } from "@prisma/client";
 import React from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 
-const CreateNoteForm = ({ id }: { id: number }) => {
-  const router = useRouter();
-
-  interface MemberNote {
-    id: number;
-    description: string;
-    authorId: number;
-  }
-
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<MemberNote>({});
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      const { data: newNote } = await axios.post(`/api/members/${id}/`, data, {
-        headers: {
-          "Cache-Control": "no-store",
-        },
-      });
-      router.push(`/members/${newNote.id}/`);
-      console.log(newNote.id);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-  return (
-    <div>
-      <form className="max-w-xl" onSubmit={onSubmit}>
-        <TextArea
-          {...register("description", { required: "This is required." })}
-          placeholder="Take notes..."
-        />
-        <Button color="indigo" variant="soft" className="mt-2">
-          Submit
-        </Button>
-      </form>
-    </div>
-  );
+const EditNote = ({ note }: { note: Note }) => {
+  return <div>{note.description}</div>;
 };
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
-export default CreateNoteForm;
+
+export default EditNote;
