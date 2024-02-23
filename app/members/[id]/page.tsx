01e5846f4f2d54bd6651b33fbb844d4972d991bf
Saro <babikiansaro@gmail.com>
2024-02-23 18:05:34 +0400

    test

--- app/members/[id]/page.tsx
+++ app/members/[id]/page.tsx
@@ -7,19 +7,19 @@ import EditNoteButton from "./EditNoteButton";
 import NoteDetails from "./NoteDetails";
 
 interface Props {
-  params: { id: number };
+  params: { id: string };
 }
 
 export default async function Page({ params }: Props) {
   const note = await prisma.note.findUnique({
-    where: { id: params.id },
+    where: { id: parseInt(params.id) },
   });
   if (!note) notFound();
 
-  // const member = await prisma.member.findUnique({
-  //   where: { id: parseInt(params.id) },
-  // });
-  // if (!member) notFound();
+  const member = await prisma.member.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!member) notFound();
 
   return (
     <div>
