14f1234c95ae9b16c72a5c4bff6748702fb24fa2
Saro <babikiansaro@gmail.com>
2024-06-28 13:14:23 -0700

    debugging

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
-import { getServerSession } from "next-auth";
+import { getServerSession, Session } from "next-auth";
 import authOptions from "../auth/authOptions";
-import { GetServerSideProps, NextPage } from "next";
-import { Session } from "next-auth";
 import { Member, Note } from "@prisma/client";
 
 interface MembersProps {
@@ -13,8 +11,8 @@ interface MembersProps {
   session: Session | null;
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
+const Members = async () => {
+  const session = await getServerSession(authOptions);
   const members = await prisma.member.findMany({
     include: {
       notes: true,
@@ -23,59 +21,59 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       createdBy: session?.user?.email || "",
     },
   });
+  if (members) {
+    return (
+      <>
+        <div>
+          <h1>Members List</h1>
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Sign Up Date</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Info</Table.ColumnHeaderCell>
+              </Table.Row>
+            </Table.Header>
 
-  return {
-    props: {
-      members,
-      session,
-    },
-  };
+            <Table.Body>
+              {members.map((member) => (
+                <Table.Row key={member.id}>
+                  <Table.RowHeaderCell>
+                    {`${member.firstname} ${member.lastname}`}
+                  </Table.RowHeaderCell>
+                  <Table.Cell>{member.createdAt.toDateString()}</Table.Cell>
+                  <Table.Cell>{member.info}</Table.Cell>
+                  <Table.Cell>
+                    {member.notes.length > 0 ? (
+                      member.notes.map((note) => (
+                        <div key={note.id}>
+                          <Button onClick={() => console.log(note.id)}>
+                            <Link href={`/members/${note.id}`}>More</Link>
+                          </Button>
+                        </div>
+                      ))
+                    ) : (
+                      <Button>
+                        <Link href={`/members/${member.id}/notes`}>
+                          Take Notes
+                        </Link>
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </div>
+
+        <Button>
+          <Link href="/">Dashboard</Link>
+        </Button>
+      </>
+    );
+  }
 };
+//export const dynamic = "force-dynamic";
 
-const Members: NextPage<MembersProps> = ({ members }) => {
-  return (
-    <div>
-      <h1>Members List</h1>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Sign Up Date</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Info</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {members.map((member) => (
-            <Table.Row key={member.id}>
-              <Table.RowHeaderCell>{`${member.firstname} ${member.lastname}`}</Table.RowHeaderCell>
-              <Table.Cell>
-                {new Date(member.createdAt).toDateString()}
-              </Table.Cell>
-              <Table.Cell>{member.info}</Table.Cell>
-              <Table.Cell>
-                {member.notes.length > 0 ? (
-                  member.notes.map((note) => (
-                    <div key={note.id}>
-                      <Link href={`/members/${note.id}`}>
-                        <Button>More</Button>
-                      </Link>
-                    </div>
-                  ))
-                ) : (
-                  <Link href={`/members/${member.id}/notes`}>
-                    <Button>Take Notes</Button>
-                  </Link>
-                )}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-      <Link href="/">
-        <Button>Dashboard</Button>
-      </Link>
-    </div>
-  );
-};
-
+export const dynamic = "force-dynamic";
 export default Members;
