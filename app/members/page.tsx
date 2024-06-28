ea14074ad9b7e66d6941c59424d09e80b5c2b2a3
Saro <babikiansaro@gmail.com>
2024-06-28 10:04:56 -0700

    debugging

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
@@ -7,66 +6,74 @@ import { getServerSession } from "next-auth";
 import authOptions from "../auth/authOptions";
 
 const Members = async () => {
-  const session = await getServerSession(authOptions);
-  const members = await prisma.member.findMany({
-    include: {
-      notes: true,
-    },
-    where: {
-      createdBy: session?.user?.email || "",
-    },
-  });
+  if (typeof window !== "undefined") {
+    const session = await getServerSession(authOptions);
+    const members = await prisma.member.findMany({
+      include: {
+        notes: true,
+      },
+      where: {
+        createdBy: session?.user?.email || "",
+      },
+    });
 
-  return (
-    <>
-      <div>
-        <h1>Members List</h1>
-        <Table.Root>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Sign Up Date</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Info</Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {members.map((member) => (
-              <Table.Row key={member.id}>
-                <Table.RowHeaderCell>
-                  {`${member.firstname} ${member.lastname}`}
-                </Table.RowHeaderCell>
-                <Table.Cell>{member.createdAt.toDateString()}</Table.Cell>
-                <Table.Cell>{member.info}</Table.Cell>
-                <Table.Cell>
-                  {member.notes.length > 0 ? (
-                    member.notes.map((note) => (
-                      <div key={note.id}>
-                        <Button onClick={() => console.log(note.id)}>
-                          <Link href={`/members/${note.id}`}>More</Link>
-                        </Button>
-                      </div>
-                    ))
-                  ) : (
-                    <Button>
-                      <Link href={`/members/${member.id}/notes`}>
-                        Take Notes
-                      </Link>
-                    </Button>
-                  )}
-                </Table.Cell>
+    return (
+      <>
+        <div>
+          <h1>Members List</h1>
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Sign Up Date</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Info</Table.ColumnHeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
-      </div>
+            </Table.Header>
 
-      <Button>
-        <Link href="/">Dashboard</Link>
-      </Button>
-    </>
-  );
+            <Table.Body>
+              {members.map((member) => (
+                <Table.Row key={member.id}>
+                  <Table.RowHeaderCell>
+                    {`${member.firstname} ${member.lastname}`}
+                  </Table.RowHeaderCell>
+                  <Table.Cell>{member.createdAt.toDateString()}</Table.Cell>
+                  <Table.Cell>{member.info}</Table.Cell>
+                  <Table.Cell>
+                    {member.notes.length > 0 ? (
+                      member.notes.map((note) => (
+                        <div key={note.id}>
+                          <Button onClick={() => console.log(note.id)}>
+                            <Link href={`/members/${note.id}`}>More</Link>
+                          </Button>
+                        </div>
+                      ))
+                    ) : (
+                      <Button>
+                        <Link href={`/members/${member.id}/notes`}>
+                          Take Notes
+                        </Link>
+                      </Button>
+                    )}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </div>
+
+        <Button>
+          <Link href="/">Dashboard</Link>
+        </Button>
+      </>
+    );
+  } else {
+    console.log(
+      "Window is undefined. This code should only run in a client-side environment."
+    );
+    return console.log("theres a problem");
+  }
 };
+
 //export const dynamic = "force-dynamic";
 
 export const dynamic = "force-dynamic";
