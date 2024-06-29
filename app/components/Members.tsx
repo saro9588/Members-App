2bd61cf26f02605da0369764b6e89749de4a7722
Saro <babikiansaro@gmail.com>
2024-06-29 14:06:32 -0700

    new prisma migration

--- app/components/Members.tsx
+++ app/components/Members.tsx
@@ -42,7 +42,6 @@ const Members: React.FC<MembersProps> = ({
               <Table.ColumnHeaderCell>Sign Up Date</Table.ColumnHeaderCell>
               <Table.ColumnHeaderCell>Info</Table.ColumnHeaderCell>
               <Table.ColumnHeaderCell>Actions</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Created By</Table.ColumnHeaderCell>
             </Table.Row>
           </Table.Header>
 
@@ -56,7 +55,6 @@ const Members: React.FC<MembersProps> = ({
                   {new Date(member.createdAt).toDateString()}
                 </Table.Cell>
                 <Table.Cell>{member.info}</Table.Cell>
-                <Table.Cell>{member.createdBy}</Table.Cell>
                 <Table.Cell>
                   {member.notes.length > 0 ? (
                     member.notes.map((note) => (
