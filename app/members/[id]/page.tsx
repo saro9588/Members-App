e9997ac052652d0905591613ad3c6cb8d65fc5cd
Saro <babikiansaro@gmail.com>
2024-06-30 13:40:22 -0700

    general styling

--- app/members/[id]/page.tsx
+++ app/members/[id]/page.tsx
@@ -25,7 +25,7 @@ export default async function Page({ params }: Props) {
   if (!member) notFound();
 
   return (
-    <div>
+    <div className="max-w-xl">
       <h1 style={{ fontWeight: "bold" }}>Member detailed notes</h1>
       <Box mb="5">
         <NoteDetails note={note} />
--- app/members/new/page.tsx
+++ app/members/new/page.tsx
@@ -10,11 +10,6 @@ const NewMemberPage = () => {
       <div className="grid grid-col-1 mx-auto max-w-screen-lg gap-2">
         <p>Create a New Member</p>
         <NewMemberForm />
-        <div>
-          <Button color="indigo" variant="soft">
-            <Link href="/members">All Members</Link>
-          </Button>
-        </div>
       </div>
     </>
   );
