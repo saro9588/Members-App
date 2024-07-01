2229220b05a2bf535a3efcf327abfccc4194f69b
Saro <babikiansaro@gmail.com>
2024-07-01 16:56:32 -0700

    fix 404 error on take notes page

--- app/members/[id]/notes/page.tsx
+++ app/members/[id]/notes/page.tsx
@@ -1,29 +1,22 @@
 import "easymde/dist/easymde.min.css";
 import NoteForm from "../../_components/NoteForm";
-import { notFound } from "next/navigation";
-import prisma from "@/prisma/client";
-import { getServerSession } from "next-auth";
-
 interface Props {
   params: { id: string };
 }
-const MemberNoteForm = async ({ params }: Props) => {
-  const session = await getServerSession();
-  const note = await prisma.note.findUnique({
-    where: { id: params.id },
-  });
-
-  if (!note || note.createdBy !== session?.user?.email) notFound();
-
-  const member = await prisma.member.findUnique({
-    where: { id: note.authorId },
-  });
-  if (!member) notFound();
+const MemberNoteForm = ({ params }: Props) => {
   return (
     <>
       <div className="grid grid-col-1 mx-auto max-w-screen-lg gap-2">
         <h1 style={{ fontWeight: "bold" }}>Member Notes</h1>
-        <NoteForm id={params.id} note={note} />
+        <NoteForm
+          id={params.id}
+          note={{
+            id: "",
+            description: "",
+            authorId: "",
+            createdBy: "",
+          }}
+        />
       </div>
     </>
   );
